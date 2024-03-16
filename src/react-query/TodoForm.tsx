5c19deed13b5a90bf1fa4b1201ce3a0de7852b7d
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-03-16 15:14:36 +0000

    show mutation progress

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -16,6 +16,7 @@ const TodoForm = () => {
       //   queryKey: ['todos'],
       // })
       queryClient.setQueryData<Todo[]>(['todos'], todos => [savedTodo, ...(todos || [])])
+      if (ref.current) ref.current.value = ''
     },
   })
   const ref = useRef<HTMLInputElement>(null)
@@ -41,7 +42,9 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>
+            {addTodo.isLoading ? 'Adding...' : 'Add'}
+          </button>
         </div>
       </form>
     </>
