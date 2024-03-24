daa209dbe89ff0ba88d0a3e703d36eed992718a7
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-03-24 12:40:38 +0000

    authReducer ok

--- src/state-management/LoginStatus.tsx
+++ src/state-management/LoginStatus.tsx
@@ -1,26 +1,28 @@
-import { useState } from "react";
+import { useReducer, useState } from 'react'
+import authReducer from './reducers/authReducer'
 
 const LoginStatus = () => {
-  const [user, setUser] = useState('');
+  // const [user, setUser] = useState('')
+  const [user, dispatch] = useReducer(authReducer, '')
 
   if (user)
     return (
       <>
         <div>
           <span className="mx-2">{user}</span>
-          <a onClick={() => setUser('')} href="#">
+          <a onClick={() => dispatch({ type: 'LOGOUT' })} href="#">
             Logout
           </a>
         </div>
       </>
-    );
+    )
   return (
     <div>
-      <a onClick={() => setUser('mosh.hamedani')} href="#">
+      <a onClick={() => dispatch({ type: 'LOGIN', username: 'nazie.sharid' })} href="#">
         Login
       </a>
     </div>
-  );
-};
+  )
+}
 
-export default LoginStatus;
+export default LoginStatus
