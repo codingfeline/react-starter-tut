774b96f92df18d9d168c777e69142cf9c7b76a77
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-03-28 21:07:47 +0000

    navigating

--- src/routing/ContactPage.tsx
+++ src/routing/ContactPage.tsx
@@ -1,14 +1,18 @@
+import { useNavigate } from 'react-router-dom'
+
 const ContactPage = () => {
+  const navigate = useNavigate()
   return (
     <form
-      onSubmit={(event) => {
-        event.preventDefault();
+      onSubmit={event => {
+        event.preventDefault()
         // Redirect the user to the home page
+        navigate('/')
       }}
     >
       <button className="btn btn-primary">Submit</button>
     </form>
-  );
-};
+  )
+}
 
-export default ContactPage;
+export default ContactPage
